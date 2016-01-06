078af417b8943e8f4583511603b6a35570a17686
Yuriy Aizenberg <yuriy.aizenberg@globus-ltd.com>
2016-01-06 19:59:12 +0300

    Added exception handling

--- src/main/java/com/aizenberg/controller/ExceptionController.java
+++ src/main/java/com/aizenberg/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.aizenberg.controller;
 
 import com.aizenberg.utils.Utils;
+import org.apache.commons.fileupload.FileUploadBase;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,12 +11,19 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class ExceptionController {
-
+//ileSizeLimitExceededException
     @ExceptionHandler(value = Exception.class)
     public ModelAndView handleThrowables(Exception exception) {
         ModelAndView modelAndView = new ModelAndView("error");
         String message = exception.getMessage();
         modelAndView.addObject("error", Utils.isEmpty(message) ? "Unknown error" : message);
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value = {FileUploadBase.FileSizeLimitExceededException.class, org.apache.tomcat.util.http.fileupload.FileUploadBase.FileSizeLimitExceededException.class})
+    public ModelAndView handleFileSizeLimit(Exception exception) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("error", "Check your file size. Maximum - 1Mb");
         return modelAndView;
     }
 
